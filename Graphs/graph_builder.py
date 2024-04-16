7caef4642a3017c86ee207aef0fa422bdd94a92a
Wool5443 <mihsolodilov2015@gmail.com>
2024-04-16 23:48:22 +0300

    Better graph_builder

--- Graphs/graph_builder.py
+++ Graphs/graph_builder.py
@@ -1,29 +1,30 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from os import listdir
+from csv import reader
 
-def hist(ys):
+def hist(ys, load_factor):
     hist_data = []
 
     for i in range(len(ys)):
         hist_data += [i] * (ys[i] + 1)
 
+    plt.title(f"Load Factor = {load_factor}")
+    plt.xlabel("Container #")
+    plt.ylabel("Load")
     plt.hist(hist_data, range(0, len(ys), int(len(ys)** 0.4)))
 
 def main():
     containers_folder = "Containers/"
-    containers_files  = [containers_folder + x for x in listdir(containers_folder) if x[-3:] == "txt"]
+    containers_files  = [containers_folder + x for x in listdir(containers_folder) if x[-3:] == "csv"]
 
     for file_path in containers_files:
         with open(file_path, "r") as f:
-            lines = f.read().split('\n')
-            xs = [0] * len(lines)
-            ys = [0] * len(lines)
-            for i, line in enumerate(lines):
-                if line:
-                    xs[i], ys[i] = map(int, line.split())
+            csv_reader = list(reader(f, delimiter='\t'))
+            ys = [list(map(int, row))[1] for row in csv_reader[:-1]]
+            load_factor = float(csv_reader[-1][0])
 
-        hist(ys)
+        hist(ys, load_factor)
         print(f"done {file_path}")
         plt.savefig(file_path + 'graph.png')
         plt.cla()
