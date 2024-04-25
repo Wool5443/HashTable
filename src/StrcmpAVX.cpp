18d7570bc7370f4e60f5b229f84f003b3c8a7286
Wool5443 <mihsolodilov2015@gmail.com>
2024-04-26 02:00:51 +0300

    Minor tweaks

--- src/StrcmpAVX.cpp
+++ src/StrcmpAVX.cpp
@@ -9,7 +9,7 @@ bool StrcmpAVX(const char* s1, const char* s2)
     __m256i s1Vector = _mm256_loadu_epi8(s1);
     __m256i s2Vector = _mm256_loadu_epi8(s2);
 
-    __m256i difference = _mm256_xor_epi32(s1Vector, s2Vector);
+    __m256i difference = _mm256_xor_epi64(s1Vector, s2Vector);
 
     __mmask32 res = _mm256_test_epi8_mask(difference, difference);
 
