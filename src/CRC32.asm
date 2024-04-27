9455d75b18d8d350891ea998aae2ff574d3e2af2
Wool5443 <mihsolodilov2015@gmail.com>
2024-04-27 10:33:03 +0300

    downgraded crc32 for better research

--- src/CRC32.asm
+++ src/CRC32.asm
@@ -9,26 +9,9 @@ section .text
 CRC32:
     cld
 
-    mov  rcx, rsi
-    and  rsi, 0b11 ; rsi = size % 4
-    mov  r8, rsi ; r8 = size % 4
+    mov  rcx, rsi ; rcx = size
     mov  rsi, rdi ; rsi -> data
     xor  rdx, rdx ; rdx holds the result
-
- 
-    shr  rcx, 2 ; rcx = size / 4
-    jz   .skip4
-
-    .loop4:
-        lodsd
-        crc32 edx, eax
-        loop .loop4
-
-    .skip4:
-    mov  rcx, r8 ; process last bytes
-
-    test rcx, rcx
-    jz   .end
 
     .loop:
         lodsb
